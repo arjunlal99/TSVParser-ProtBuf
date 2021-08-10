dfec4630cd863f66057f77730492d401c5b2b0a6
Arjunlal M A <arjunlal@nbdm11c-116.prevalent.com>
2021-08-10 15:11:52 +0530

    Adding configuration file support

--- src/main/java/TSVParser.java
+++ src/main/java/TSVParser.java
@@ -26,28 +26,92 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class TSVParser {
 
     private static final GroupFactory factory = new SimpleGroupFactory(MessageTypeParser.parseMessageType(
             "message example{\n" +
-                    "required BINARY x;\n" +
+                    "required BINARY ts;\n" +
+                    "required BINARY uid;\n" +
+                    "required BINARY id.orig_h;\n" +
+                    "required BINARY id.orig_p;\n" +
+                    "required BINARY id.resp_h;\n" +
+                    "required BINARY id.resp_p;\n" +
+                    "required BINARY mac;\n" +
+                    "required BINARY assigned_ip;\n" +
+                    "required BINARY lease_time;\n" +
+                    "required BINARY trans_id;\n" +
                     "}"
     ));
 
     public static class TSVParserMapper extends Mapper<LongWritable, Text, Void, Group>{
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
-            Group group = factory.newGroup().append("x", value.toString());
+            String line = value.toString();
+            String[] fields = line.split("\t");
+            Group group = factory.newGroup();
+            group.append("ts", fields[0]);
+            group.append("uid", fields[1]);
+            group.append("id.orig_h", fields[2]);
+            group.append("id.orig_p", fields[3]);
+            group.append("id.resp_h", fields[4]);
+            group.append("id.resp_p", fields[5]);
+            group.append("mac", fields[6]);
+            group.append("assigned_ip", fields[7]);
+            group.append("lease_time", fields[8]);
+            group.append("trans_id", fields[9]);
 
             context.write(null, group);
         }
 
     }
 
+    public static class ConfParser{
+
+       public static String inputDir, outputDir, outputFilename;
+       public static JSONArray fields;
+/*
+    path -> absolute path
+
+    conf.json
+    {
+        "inputDir": "/input",
+        "outputDir": "/output",
+        "outputFilename": "dhcp-log",
+        "fields": []
+}
+
+ */
+       public ConfParser(String path){
+            JSONParser jsonParser = new JSONParser();
+            try(FileReader reader = new FileReader(path)){
+                Object obj = jsonParser.parse(reader);
+                JSONObject file = (JSONObject) obj;
+                ConfParser.inputDir = file.get("inputDir").toString();
+                ConfParser.outputDir = file.get("outputDir").toString();
+                ConfParser.outputFilename = file.get("outputFilename").toString();
+                ConfParser.fields = (JSONArray) file.get("fields");
+            }
+            catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            catch (ParseException e){
+                e.printStackTrace();
+            }
+       }
+    }
+
     public static void main (String [] args) throws Exception{
-
+        ConfParser confParser = new ConfParser(args[0]);
         Job job = Job.getInstance(new Configuration(), "TSVParser");
-
+        job.getConfiguration().set("mapreduce.output.basename", ConfParser.outputFilename);
         job.setJarByClass(TSVParser.class);
         job.setMapperClass(TSVParserMapper.class);
         job.setNumReduceTasks(0);
@@ -57,14 +121,24 @@ public class TSVParser {
 
         ExampleOutputFormat.setSchema(job,MessageTypeParser.parseMessageType(
                 "message example{\n" +
-                        "required BINARY x;\n" +
+                        "required BINARY ts;\n" +
+                        "required BINARY uid;\n" +
+                        "required BINARY id.orig_h;\n" +
+                        "required BINARY id.orig_p;\n" +
+                        "required BINARY id.resp_h;\n" +
+                        "required BINARY id.resp_p;\n" +
+                        "required BINARY mac;\n" +
+                        "required BINARY assigned_ip;\n" +
+                        "required BINARY lease_time;\n" +
+                        "required BINARY trans_id;\n" +
                         "}"
         ) );
 
         ExampleOutputFormat.setCompression(job, CompressionCodecName.UNCOMPRESSED);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(ConfParser.inputDir));
+        FileOutputFormat.setOutputPath(job, new Path(ConfParser.outputDir));
+
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
